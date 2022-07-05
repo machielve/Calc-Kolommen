b7a9c4c5092ebf2affa889b822019b0befb3cd17
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-07-05 09:11:23 +0200

    Go

--- Inkoop/Levering inkooporder.cs
+++ Inkoop/Levering inkooporder.cs
@@ -1,3 +1,4 @@
 If([Incoterm].[Code]=="DAP" AND [Los adres]==1098, "Levering Almacon", "")
-+If([Incoterm].[Code]=="DAP" AND [Los adres]<>1098 AND [Los adres].[Relatie].[Bedrijfstak].[Code]=="COATER", "Levering coater", If([Incoterm].[Code]=="DAP" AND [Los adres]<>1098 , "Levering on site", ""))
++If([Incoterm].[Code]=="DAP" AND [Los adres]<>1098 AND [Los adres].[Relatie].[Bedrijfstak].[Code]=="COATER", "Levering coater", 
+        If([Incoterm].[Code]=="DAP" AND [Los adres]<>1098 , "Levering on site", ""))
 +If([Incoterm].[Code]<>"DAP", "Af halen", "")
