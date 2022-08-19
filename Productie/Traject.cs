f03f3ccbc57b3f8aa3729cc3ed0d30e7a171a305
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-08-19 11:49:23 +0200

    Go

--- Productie/Traject.cs
+++ Productie/Traject.cs
@@ -2,4 +2,4 @@ If([Artikel].[Voorraadtype]=="1"                                                
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1"                                                , "Bestellen"       , "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   , "Voorraad"        , "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]>[@vrije voorraad] , "Productie ALM"   , "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]<=[@vrije voorraad] , "Productie ALM"   , "")
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]<=[@vrije voorraad] , "Voorraad"   , "")
