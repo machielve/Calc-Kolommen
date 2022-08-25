8996dde28c699b0f6af74b8dd9fc394dbac35566
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-08-25 09:25:45 +0200

    Go

--- Productie/Traject.cs
+++ Productie/Traject.cs
@@ -1,6 +1,7 @@
-If([Artikel].[Voorraadtype]=="1"                                                                    , "Bestellen"       , "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1"                                                , "Bestellen"       , "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"  AND [Magazijn].[Magazijn]==4 ,       "Externe voorraad", "")+
+If([Artikel].[Voorraadtype]=="1"                                                                    ,                                   "Bestellen", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1"                                                ,                                   "Bestellen", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"  AND [Magazijn].[Magazijn]==4 ,       "Externe voorraad BCB", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"  AND [Magazijn].[Magazijn]==2,        "Voorraad", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]<=[@vrije voorraad] ,    "Voorraad", "")
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]<=[@vrije voorraad] ,    "Voorraad", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"  AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", "")
+
