e3648c8c7f6958d06d09ce1420891193b0b7ead6
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-08-29 17:03:48 +0200

    Go

---
+++ Stuklijsten/Afwerking check.cs
@@ -0,0 +1,26 @@
+IF(
+(
+COUNTCONDITION([Stuklijstregel artikel ID],   
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]=="2" AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==[Afwijkende leverancier] AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==[Artikel] AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==[Stuklijstregel artikel ID])
+
++
+
+COUNTCONDITION([Stuklijstregel artikel ID], 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]=="2" AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==[Artikel].[Hoofdleverancier].[Relatie] AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==[Artikel] AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==[Stuklijstregel artikel ID] AND
+
+IsEmpty([Afwijkende leverancier]))
+
+-
+
+COUNTCONDITION([Detail_Stuklijst artikel uitbesteed werk_KMB stuklijstregel artikel].[KMB stuklijstregel uitbesteed werk].[Stuklijstregel uitbesteed ID], 
+Left([Detail_Stuklijst artikel uitbesteed werk_KMB stuklijstregel artikel].[KMB stuklijstregel uitbesteed werk].[Uitbesteed werk].[Code], 3)=="U10")
+
+)>0,"Afwerking toevoegen","")
+
+
