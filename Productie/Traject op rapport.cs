17169f0c103b44e3873e0176ff4dc27caf3c19e7
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-08-31 15:35:35 +0200

    Go

--- Productie/Traject op rapport.cs
+++ Productie/Traject op rapport.cs
@@ -1,6 +1,6 @@
 If([Artikel].[Voorraadtype]=="1",                                                                                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1",                                                                               "Bestellen", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]<>"3" AND [Magazijn].[Magazijn]==4,      "Uit voorraad bij BCB-Almacon", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]<>"3" AND [Magazijn].[Magazijn]==2,      "Pakken uit magazijn Almacon", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]=="3" AND [Aantal]<=[@vrije voorraad],   "Pakken uit magazijn Almacon", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]=="3" AND [Aantal]>[@vrije voorraad],    "Productie aangestuurd door Almacon", "")
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==4,      "Uit voorraad bij BCB-Almacon", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2,      "Pakken uit magazijn Almacon", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]<=[@vrije voorraad],   "Pakken uit magazijn Almacon", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad],    "Productie aangestuurd door Almacon", "")
