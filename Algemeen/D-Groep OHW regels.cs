3b6192c620c5921e3eff2068432496d7285827fc
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-09-07 16:17:03 +0200

    Go

---
+++ Algemeen/D-Groep OHW regels.cs
@@ -0,0 +1,10 @@
+If(Left([Code], 1) =="T"                                                            , "Transport", "")+
+If(Left([Code], 3) =="D10" OR Left([Code], 4)=="K002" OR Left([Code], 4)=="K003"    , "Kolommen", "")+
+if(Left([Code], 3) =="D20"                                                          , "Koud gewalste liggers", "")+
+If(Left([Code], 3) =="D25" OR Left([Code], 4)=="K001"                               , "Warm gewalste liggers", "")+
+If(Left([Code], 3) =="D30" OR Left([Code], 4)=="M002"                               , "Vloerdelen", "")+
+if(Left([Code], 3) =="D40"                                                          , "Trappen", "")+
+if(Left([Code], 3) =="D50"                                                          , "Leuning", "")+
+if(Left([Code], 3) =="D51"                                                          , "Hekwerk", "")+
+if(Left([Code], 3) =="D55"                                                          , "Pallet opzet plaatsen", "")+
+If(Left([Code], 3) =="D80"                                                          , "Montage", "")
