07435faf162daf4e02235c65132a065153de8abd
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-09-09 15:38:13 +0200

    Go

--- Stuklijsten/Afwerking check.cs
+++ Stuklijsten/Afwerking check.cs
@@ -1,18 +1,18 @@
 IF(
 (
 COUNTCONDITION([Stuklijstregel artikel ID],   
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]=="2" AND
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==[Afwijkende leverancier] AND
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==[Artikel] AND
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==[Stuklijstregel artikel ID])
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]==       "2" AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==            [Afwijkende leverancier] AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==            [Artikel] AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==  [Stuklijstregel artikel ID])
 
 +
 
 COUNTCONDITION([Stuklijstregel artikel ID], 
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]=="2" AND 
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==[Artikel].[Hoofdleverancier].[Relatie] AND 
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==[Artikel] AND 
-[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==[Stuklijstregel artikel ID] AND
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel type]==       "2" AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Relatie]==            [Artikel].[Hoofdleverancier].[Relatie] AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel]==            [Artikel] AND 
+[Artikel].[Detail_Artikelen per leverancier_Artikel].[Artikel].[Detail_Stuklijstregel artikel_Artikel].[Stuklijstregel artikel ID]==   [Stuklijstregel artikel ID] AND
 
 IsEmpty([Afwijkende leverancier]))
 
