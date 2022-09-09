293f5f4390149f62830de659d34fefade83d9d43
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-09-09 14:32:42 +0200

    Go

---
+++ Productie/Doel bon.cs
@@ -0,0 +1,6 @@
+ConvertIntToString(MAX([Detail_Alle reserveringen_Herkomstbon].[Bestemmings bon].[Order].[Ordernummer]))
+
++"."+
+
+ConvertIntToString(MAX([Detail_Alle reserveringen_Herkomstbon].[Bestemmings bon].[Bonnummer]))
+
