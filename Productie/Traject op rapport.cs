f815112a6158df04b5664273e1cc068e8312976e
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-09-13 15:09:58 +0200

    Go

--- Productie/Traject op rapport.cs
+++ Productie/Traject op rapport.cs
@@ -1,6 +1,12 @@
 If([Artikel].[Voorraadtype]=="1",                                                                                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1",                                                                               "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==4,      "Uit voorraad bij BCB-Almacon", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2,      "Pakken uit magazijn Almacon", "")+
+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2 
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")>0,      "Productie aangestuurd door Almacon", "")+
+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2 
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")==0,      "Pakken uit magazijn Almacon", "")+
+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]<=[@vrije voorraad],   "Pakken uit magazijn Almacon", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad],    "Productie aangestuurd door Almacon", "")
--- Productie/Traject.cs
+++ Productie/Traject.cs
@@ -1,7 +1,13 @@
 If([Artikel].[Voorraadtype]=="1"                                                                    ,                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1"                                                ,                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==4 ,       "Externe voorraad BCB", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2,        "Voorraad", "")+
+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]<>"3" AND [Magazijn].[Magazijn]==2 
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")>0, "Productie ALM", "")+
+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2 
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")==0,        "Voorraad", "")+
+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]<=[@vrije voorraad] ,    "Voorraad", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", "")
 
