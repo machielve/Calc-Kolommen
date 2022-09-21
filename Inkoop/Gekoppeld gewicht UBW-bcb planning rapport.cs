4e4a87802e6a0311dc89e2f78663421f6f9ffe34
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-09-21 08:36:01 +0200

    Go

---
+++ Inkoop/Gekoppeld gewicht UBW-bcb planning rapport.cs
@@ -0,0 +1,3 @@
+SUM([Inkooporderregel uitbesteed werk].[Detail_Alle reserveringen_Inkooporderregel uitbesteed werk].[Bonregel uitbesteed werk].[Detail_Bonregel artikel uitbesteed werk_KMB bonregel uitbesteed werk].[KMB bonregel artikel].[Totaal gewicht])
+*
+SUM([Inkooporderregel uitbesteed werk].[Detail_Alle reserveringen_Inkooporderregel uitbesteed werk].[Bonregel uitbesteed werk].[Bon].[Aantal gepland])
---
+++ Inkoop/gekoppeld gewicht UBW.cs
@@ -0,0 +1,3 @@
+SUM([Detail_Alle reserveringen_Inkooporderregel uitbesteed werk].[Bonregel uitbesteed werk].[Detail_Bonregel artikel uitbesteed werk_KMB bonregel uitbesteed werk].[KMB bonregel artikel].[Totaal gewicht])
+*
+SUM([Detail_Alle reserveringen_Inkooporderregel uitbesteed werk].[Bonregel uitbesteed werk].[Bon].[Aantal gepland])
