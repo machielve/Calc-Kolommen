b76e388961e9ad557e94f7fe98273e029d7f3286
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-18 15:53:35 +0100

    Go

---
+++ Productie/Productiestatus.cs
@@ -0,0 +1,37 @@
+If([@Productieregels]==0, "Geen productie", "Lopende productie")
+
+
+
+
+
+
+// hieronder de visuele scripting regel
+using Ridder.Client.UIScript;
+using System.Drawing;
+using Ridder.Common.Script;
+using System;
+
+public class RidderScript : ConditionalFormatScript
+{
+	public void Execute()
+
+	{
+		DateTime now = DateTime.Today;
+
+		if (this.Item.Value.ToString() == "Geen productie")
+		{
+			this.Item.Control.SetBackgroundColor(Color.Orange);
+		}
+
+
+		else if (this.Item.Value.ToString() == "Lopende productie" && FindItem("VZD").Value.ToDateTime().Date >= now)
+		{
+			this.Item.Control.SetBackgroundColor(Color.Yellow);
+		}
+
+		else if (this.Item.Value.ToString() == "Lopende productie" && FindItem("VZD").Value.ToDateTime().Date < now)
+		{
+			this.Item.Control.SetBackgroundColor(Color.Red);
+		}
+	}
+}
