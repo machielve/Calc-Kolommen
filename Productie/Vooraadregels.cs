c7733299e8691690f3f1a288b5763919c77b1b74
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-21 08:34:06 +0100

    Go

---
+++ Productie/Vooraadregels.cs
@@ -0,0 +1,8 @@
+COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaal ID], 
+
+[Detail_Materiaalstatus_Bon].[Voorraadtype]<>"1" AND 
+[Detail_Materiaalstatus_Bon].[Materiaaltype]=="1" AND
+
+[Detail_Materiaalstatus_Bon].[Artikelmagazijn].[Artikel].[Artikeleenheid].[Soort]<>"3"
+
+)
