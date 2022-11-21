ce68925b6bd88b36e516348daf7c5e77313728e6
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-21 10:22:01 +0100

    Go

--- Productie/Pickregels.cs
+++ Productie/Pickregels.cs
@@ -1,5 +1,6 @@
 COUNTCONDITION(
 [Detail_Materiaalstatus_Bon].[Materiaal ID], 
+
 [Detail_Materiaalstatus_Bon].[Voorraadtype]<>"1" AND 
 [Detail_Materiaalstatus_Bon].[Materiaaltype]=="1" AND 
 [Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Waar] AND
