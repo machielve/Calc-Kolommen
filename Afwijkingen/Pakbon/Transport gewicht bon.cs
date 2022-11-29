c77836a321e230b61c9c916cb2971601e16f6b7b
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-29 11:39:29 +0100

    Go

---
+++ Afwijkingen/Pakbon/Transport gewicht bon.cs
@@ -0,0 +1,4 @@
+SUMCONDITION(
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal], 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Detail_Alle reserveringen_Inkooporderregel divers].[Bonregel divers].[Bon]==[Bon] AND 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0003")
---
+++ Afwijkingen/Pakbon/Transport gewicht.cs
@@ -0,0 +1,3 @@
+SUMCONDITION(
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal], 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0003")
