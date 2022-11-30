30e156d1272e4dbd408316b1909e2c9149636580
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-11-30 11:59:20 +0100

    Go

--- Productie/Ontvangstregels.cs
+++ Productie/Ontvangstregels.cs
@@ -4,7 +4,7 @@ COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaalstatus id],
 [Detail_Materiaalstatus_Bon].[Ontvangen]<>[$MaterialState.Onwaar])+
 
 
-OUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaalstatus id], 
+COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaalstatus id], 
 
 [Detail_Materiaalstatus_Bon].[Voorraadtype]=="1" AND 
 [Detail_Materiaalstatus_Bon].[Ontvangen]<>[$MaterialState.Onwaar])
