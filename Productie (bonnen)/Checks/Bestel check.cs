57de89ed53091bd576eb3bf27772e0b735db93fb
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 16:02:45 +0100

    Go

--- Productie (bonnen)/Checks/Bestel check.cs
+++ Productie (bonnen)/Checks/Bestel check.cs
@@ -1,7 +1,4 @@
-If(
-    
-COUNTCONDITION(
-
-[Detail_Materiaalstatus_Bon].[Materiaalstatus id], 
-
-[Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Onwaar] AND [Detail_Materiaalstatus_Bon].[Voorraadtype]==[$InventoryKind.Niet voorraadhoudend])>0, "Niet alles besteld", "")
+If( COUNTCONDITION(
+[Detail_Materiaalstatus_Bon].[Materiaalstatus id],
+[Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Onwaar] AND 
+[Detail_Materiaalstatus_Bon].[Voorraadtype]==[$InventoryKind.Niet voorraadhoudend])>0, "Niet alles besteld", "")
