0f72b7ef99f52c09002e62c7d35b73747fd7a076
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 17:01:22 +0100

    Go

--- OHW/D groep.cs
+++ OHW/D groep.cs
@@ -1,13 +1,13 @@
 If(Left([Code], 1) =="T"                                                            , "Transport", "")+
-if(Left([Code], 1) =="A"                                                            , "Administratief", "")+
-if(Left([Code], 1) =="B"                                                            , "Uitbesteed werk", "")+
+If(Left([Code], 1) =="A"                                                            , "Administratief", "")+
+If(Left([Code], 1) =="B"                                                            , "Uitbesteed werk", "")+
 If(Left([Code], 3) =="D10" OR Left([Code], 4)=="K002" OR Left([Code], 4)=="K003"    , "Kolommen", "")+
-if(Left([Code], 3) =="D15"                                                          , "Schoring", "")+
-if(Left([Code], 3) =="D20" OR Left([Code], 4)=="M001"                               , "Koud gewalste liggers", "")+
+If(Left([Code], 3) =="D15"                                                          , "Schoring", "")+
+If(Left([Code], 3) =="D20" OR Left([Code], 4)=="M001"                               , "Koud gewalste liggers", "")+
 If(Left([Code], 3) =="D25" OR Left([Code], 4)=="K001"                               , "Warm gewalste liggers", "")+
 If(Left([Code], 3) =="D30" OR Left([Code], 4)=="M002"                               , "Vloerdelen", "")+
-if(Left([Code], 3) =="D40"                                                          , "Trappen", "")+
-if(Left([Code], 3) =="D50"                                                          , "Leuning", "")+
-if(Left([Code], 3) =="D51"                                                          , "Hekwerk", "")+
-if(Left([Code], 3) =="D55"                                                          , "Pallet opzet plaatsen", "")+
+If(Left([Code], 3) =="D40"                                                          , "Trappen", "")+
+If(Left([Code], 3) =="D50"                                                          , "Leuning", "")+
+If(Left([Code], 3) =="D51"                                                          , "Hekwerk", "")+
+If(Left([Code], 3) =="D55"                                                          , "Pallet opzet plaatsen", "")+
 If(Left([Code], 3) =="D80"                                                          , "Montage", "")
