643ab0265eb30abedb935cd9a358d738a9b44271
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 16:30:42 +0100

    Go

--- Algemeen/D-Groep OHW regels.cs
+++ Algemeen/D-Groep OHW regels.cs
@@ -2,7 +2,7 @@ If(Left([Code], 1) =="T"                                                        
 if(Left([Code], 1) =="A"                                                            , "Administratief", "")+
 if(Left([Code], 1) =="B"                                                            , "Bewerkingen", "")+
 If(Left([Code], 3) =="D10" OR Left([Code], 4)=="K002" OR Left([Code], 4)=="K003"    , "Kolommen", "")+
-if(Left([Code], 3) =="D20"                                                          , "Koud gewalste liggers", "")+
+if(Left([Code], 3) =="D20" OR Left([Code], 4)=="M001"                               , "Koud gewalste liggers", "")+
 If(Left([Code], 3) =="D25" OR Left([Code], 4)=="K001"                               , "Warm gewalste liggers", "")+
 If(Left([Code], 3) =="D30" OR Left([Code], 4)=="M002"                               , "Vloerdelen", "")+
 if(Left([Code], 3) =="D40"                                                          , "Trappen", "")+
