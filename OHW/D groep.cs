b9b958115adbc1e7fd02fc766acbaeacba102030
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 17:07:45 +0100

    Go

--- OHW/D groep.cs
+++ OHW/D groep.cs
@@ -1,13 +1,24 @@
 If(Left([Code], 1) =="T"                                                            , "Transport", "")+
 If(Left([Code], 1) =="A"                                                            , "Documentatie", "")+
 If(Left([Code], 1) =="B"                                                            , "Uitbesteed werk", "")+
-If(Left([Code], 3) =="D10" OR Left([Code], 4)=="K002" OR Left([Code], 4)=="K003"    , "Kolommen", "")+
+
+If(Left([Code], 3) =="D10"                                                          , "Kolommen", "")+
 If(Left([Code], 3) =="D15"                                                          , "Schoring", "")+
-If(Left([Code], 3) =="D20" OR Left([Code], 4)=="M001"                               , "Koud gewalste liggers", "")+
-If(Left([Code], 3) =="D25" OR Left([Code], 4)=="K001"                               , "Warm gewalste liggers", "")+
-If(Left([Code], 3) =="D30" OR Left([Code], 4)=="M002"                               , "Vloerdelen", "")+
+If(Left([Code], 3) =="D20"                                                          , "Koud gewalste liggers", "")+
+If(Left([Code], 3) =="D25"                                                          , "Warm gewalste liggers", "")+
+If(Left([Code], 3) =="D30"                                                          , "Vloerdelen", "")+
 If(Left([Code], 3) =="D40"                                                          , "Trappen", "")+
 If(Left([Code], 3) =="D50"                                                          , "Leuning", "")+
 If(Left([Code], 3) =="D51"                                                          , "Hekwerk", "")+
 If(Left([Code], 3) =="D55"                                                          , "Pallet opzet plaatsen", "")+
-If(Left([Code], 3) =="D80"                                                          , "Montage", "")
+If(Left([Code], 3) =="D70"                                                          , "Bevestigingsmaterialen", "")+
+If(Left([Code], 3) =="D75"                                                          , "Accessoires", "")+
+If(Left([Code], 3) =="D80"                                                          , "Montage", "")+
+If(Left([Code], 3) =="D85"                                                          , "Uitbesteed werk", "")+
+
+If(Left([Code], 4) =="K001"                                                         , "Warm gewalste liggers", "")+
+If(Left([Code], 4) =="K002"                                                         , "Kolommen", "")+
+If(Left([Code], 4) =="K003"                                                         , "Kolommen", "")+
+
+If(Left([Code], 4) =="M001"                                                         , "Koud gewalste liggers", "")+
+If(Left([Code], 4) =="M002"                                                         , "Vloerdelen", "")
