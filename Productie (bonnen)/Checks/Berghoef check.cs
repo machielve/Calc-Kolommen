ed376e56199ddfc098ee4fd98d3562bee140d556
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 16:50:30 +0100

    Go

--- Productie (bonnen)/Checks/Berghoef check.cs
+++ Productie (bonnen)/Checks/Berghoef check.cs
@@ -1,8 +1,6 @@
 If(COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaal ID], 
 
-Left([Detail_Materiaalstatus_Bon].[Inkoopbestelling].[Leverancier].[Code], 8)=="2002")>0 
-
-AND COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaal ID], 
-
+Left([Detail_Materiaalstatus_Bon].[Inkoopbestelling].[Leverancier].[Code], 8)=="2002")>0 AND 
+COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaal ID], 
 Left([Detail_Materiaalstatus_Bon].[Code], 5)=="T0005" AND 
-        [Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Waar])==0, "Berghoef zonder transport", "")
+[Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Waar])==0, "Berghoef zonder transport", "")
