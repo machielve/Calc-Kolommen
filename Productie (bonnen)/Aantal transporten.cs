0e72c5d4a55c78d75f55522fcf6c67d4eca30bc2
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-08 16:18:49 +0100

    Go

--- Productie (bonnen)/Aantal transporten.cs
+++
@@ -1,3 +0,0 @@
-COUNTCONDITION([Detail_Materiaalstatus_Bon].[Materiaal ID], 
-
-[Detail_Materiaalstatus_Bon].[Besteld]==[$MaterialState.Waar] AND Left([Detail_Materiaalstatus_Bon].[Code], 5)=="T0003")
