127ca21c42520556e3f84ec56814f19bebd8bc6a
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-09 09:55:36 +0100

    Go

---
+++ OHW (Na-Calc)/U groep.cs
@@ -0,0 +1,5 @@
+If(Left([Code], 3) =="U10"          , "Coaten", "")+
+If(Left([Code], 3) =="U15"          , "Constructie algemeen", "")+
+If(Left([Code], 3) =="U16"          , "Constructie algemeen", "")+
+If(Left([Code], 3) =="U20"          , "Trappen", "")+
+If(Left([Code], 3) =="U90"          , "Rest", "")
