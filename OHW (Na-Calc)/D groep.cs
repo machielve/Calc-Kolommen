f14ad6e1c9affb1db036ce25a828079aeae3202e
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2022-12-14 09:18:08 +0100

    Go

--- OHW (Na-Calc)/D groep.cs
+++ OHW (Na-Calc)/D groep.cs
@@ -1,26 +1,26 @@
-If(Left([Code], 1) =="T"            , "Transport", "")+
-If(Left([Code], 1) =="A"            , "Documentatie", "")+
-If(Left([Code], 1) =="B"            , "Bewerkingen", "")+
+If(Left([Code], 1) =="T"      , "Transport", "")+
+If(Left([Code], 1) =="A"      , "Documentatie", "")+
+If(Left([Code], 1) =="B"      , "Bewerkingen", "")+
 
-If(Left([Code], 3) =="D10"          , "Kolommen", "")+
-If(Left([Code], 3) =="D15"          , "Schoring", "")+
-If(Left([Code], 3) =="D20"          , "Koud gewalste liggers", "")+
-If(Left([Code], 3) =="D25"          , "Warm gewalste liggers", "")+
-If(Left([Code], 3) =="D30"          , "Vloerdelen", "")+
-If(Left([Code], 3) =="D40"          , "Trappen", "")+
-If(Left([Code], 3) =="D50"          , "Leuning", "")+
-If(Left([Code], 3) =="D51"          , "Hekwerk", "")+
-If(Left([Code], 3) =="D55"          , "Pallet opzet plaatsen", "")+
-If(Left([Code], 3) =="D60"          , "Accessoires constructie", "")+
-If(Left([Code], 3) =="D70"          , "Bevestigingsmaterialen", "")+
-If(Left([Code], 3) =="D75"          , "Accessoires", "")+
-If(Left([Code], 3) =="D80"          , "Montage", "")+
-If(Left([Code], 3) =="D85"          , "Bewerkingen", "")+
-If(Left([Code], 2) =="D9"           , "Rest", "")+
+If(Left([Code], 3) =="D10"    , "Kolommen", "")+
+If(Left([Code], 3) =="D15"    , "Schoring", "")+
+If(Left([Code], 3) =="D20"    , "Koud gewalste liggers", "")+
+If(Left([Code], 3) =="D25"    , "Warm gewalste liggers", "")+
+If(Left([Code], 3) =="D30"    , "Vloerdelen", "")+
+If(Left([Code], 3) =="D40"    , "Trappen", "")+
+If(Left([Code], 3) =="D50"    , "Leuning", "")+
+If(Left([Code], 3) =="D51"    , "Hekwerk", "")+
+If(Left([Code], 3) =="D55"    , "Pallet opzet plaatsen", "")+
+If(Left([Code], 3) =="D60"    , "Accessoires constructie", "")+
+If(Left([Code], 3) =="D70"    , "Bevestigingsmaterialen", "")+
+If(Left([Code], 3) =="D75"    , "Accessoires", "")+
+If(Left([Code], 3) =="D80"    , "Montage", "")+
+If(Left([Code], 3) =="D85"    , "Bewerkingen", "")+
+If(Left([Code], 2) =="D9"     , "Rest", "")+
 
-If(Left([Code], 4) =="K001"         , "Warm gewalste liggers", "")+
-If(Left([Code], 4) =="K002"         , "Kolommen", "")+
-If(Left([Code], 4) =="K003"         , "Kolommen", "")+
+If(Left([Code], 4) =="K001"   , "Warm gewalste liggers", "")+
+If(Left([Code], 4) =="K002"   , "Kolommen", "")+
+If(Left([Code], 4) =="K003"   , "Kolommen", "")+
 
-If(Left([Code], 4) =="M001"         , "Koud gewalste liggers", "")+
-If(Left([Code], 4) =="M002"         , "Vloerdelen", "")
+If(Left([Code], 4) =="M001"   , "Koud gewalste liggers", "")+
+If(Left([Code], 4) =="M002"   , "Vloerdelen", "")
