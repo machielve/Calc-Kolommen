ef13cc59e6e53f35519a8ef36996fc2e3f2846cd
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-01-26 15:56:37 +0100

    Go

--- Termijncode BTWcode omzetcode checks/BTWcode check offerte.cs
+++ Termijncode BTWcode omzetcode checks/BTWcode check offerte.cs
@@ -1,5 +1,6 @@
 If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NL"    AND [BTW-bedrijfsgroep].[Code]=="NL")
     OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AE"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="GB"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AW"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NO"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="UA"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
@@ -8,6 +9,7 @@ If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]==
     
     OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NL"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"AE"
+                                  AND [Bestemmingsadres].[Adres].[Land].[Code]<>"GB"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"AW"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NO"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"UA"
@@ -22,6 +24,7 @@ If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]==
 
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NL"       AND [BTW-bedrijfsgroep].[Code]=="NL")
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AE"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="GB"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AW"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NO"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="US"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
@@ -29,6 +32,7 @@ If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]==
     
     OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]<>"NL" 
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"AE"
+                                  AND [Relatie].[Bezoekadres].[Land].[Code]<>"GB"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"AW"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"NO"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"UA"
--- Termijncode BTWcode omzetcode checks/Omzetcode check order.cs
+++ Termijncode BTWcode omzetcode checks/Omzetcode check order.cs
@@ -1,38 +1,42 @@
-If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NL"    AND [Omzetcode].[Code]=="NL")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AE"    AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AW"    AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NO"    AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="UA"    AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="US"    AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="TR"    AND [Omzetcode].[Code]=="BEU")
+If(     ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NL"    AND [BTW-bedrijfsgroep].[Code]=="NL")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AE"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="GB"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="AW"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="NO"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="UA"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="US"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]=="TR"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
     
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NL" 
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NL"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"AE"
+                                  AND [Bestemmingsadres].[Adres].[Land].[Code]<>"GB"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"AW"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NO"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"UA"
                                   AND [Bestemmingsadres].[Adres].[Land].[Code]<>"TR"
-                                  AND [Bestemmingsadres].[Adres].[Land].[Code]<>"US"    AND [Omzetcode].[Code]<>"NL"
-                                                                                        AND [Omzetcode].[Code]<>"BEU")
+                                  AND [Bestemmingsadres].[Adres].[Land].[Code]<>"US"    AND [BTW-bedrijfsgroep].[Code]<>"NL"
+                                                                                        AND [BTW-bedrijfsgroep].[Code]<>"BEU")
                                   
-    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NL"    AND [Relatie].[Bezoekadres].[Land].[Code]=="NL" AND [Omzetcode].[Code]=="NL")
-    OR  ([Incoterm].[Code]=="DAP" AND Left([Bestemmingsadres].[Naam],9)=="Foldy Pac"    AND [Omzetcode].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="DAP" AND [Bestemmingsadres].[Adres].[Land].[Code]<>"NL"    AND [Relatie].[Bezoekadres].[Land].[Code]=="NL" AND [BTW-bedrijfsgroep].[Code]=="NL")
+    OR  ([Incoterm].[Code]=="DAP" AND Left([Bestemmingsadres].[Naam],9)=="Foldy Pac"    AND [BTW-bedrijfsgroep].[Code]=="BEU")
+
+    OR  ([Incoterm].[Code]=="FCA" AND [Relatie].[Bezoekadres].[Land].[Code]=="UA"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NL"       AND [BTW-bedrijfsgroep].[Code]=="NL")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AE"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="GB"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AW"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NO"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="US"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="TR"       AND [BTW-bedrijfsgroep].[Code]=="BEU")
     
-    OR  ([Incoterm].[Code]=="FCA" AND [Relatie].[Bezoekadres].[Land].[Code]=="UA"       AND [Omzetcode].[Code]=="BEU")
-        
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NL"       AND [Omzetcode].[Code]=="NL")
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AE"       AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="AW"       AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="NO"       AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="US"       AND [Omzetcode].[Code]=="BEU")
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]=="TR"       AND [Omzetcode].[Code]=="BEU")
-    
-    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]<>"NL"
+    OR  ([Incoterm].[Code]=="EXW" AND [Relatie].[Bezoekadres].[Land].[Code]<>"NL" 
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"AE"
+                                  AND [Relatie].[Bezoekadres].[Land].[Code]<>"GB"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"AW"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"NO"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"UA"
                                   AND [Relatie].[Bezoekadres].[Land].[Code]<>"TR"
-                                  AND [Relatie].[Bezoekadres].[Land].[Code]<>"US"       AND [Omzetcode].[Code]<>"NL"
-                                                                                        AND [Omzetcode].[Code]<>"BEU")
-                                                                                                                        ,"Omzetcode correct", "Omzetcode fout")
+                                  AND [Relatie].[Bezoekadres].[Land].[Code]<>"US"       AND [BTW-bedrijfsgroep].[Code]<>"NL"
+                                                                                        AND [BTW-bedrijfsgroep].[Code]<>"BEU")
+                                                                                                                        , "BTWcode correct", "BTWcode fout")
