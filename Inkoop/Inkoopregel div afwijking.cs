0a2460007e15972a4226fae410abd6be46004573
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-03-08 09:42:32 +0100

    Go

---
+++ Inkoop/Inkoopregel div afwijking.cs
@@ -0,0 +1,6 @@
+If(
+[Bonregel divers].[Aantal]*1.05<[Aantal] OR
+[Bonregel divers].[Aantal]*0.95>[Aantal]
+, 
+
+"Afwijking groter dan 5%", "")
---
+++ Order/NaCalc per bon.cs
@@ -0,0 +1,10 @@
+SUM([Detail_OHW artikelcorrecties_Bon].[Kostprijs correctie])+
+SUM([Detail_OHW artikelen_Bon].[Totaal kostprijs])+
+
+SUM([Detail_OHW diversen_Bon].[Totaal kostprijs])+
+SUM([Detail_OHW diversencorrecties_Bon].[Kostprijs correctie])+
+
+
+
+SUM([Detail_OHW uitbesteed werk_Bon].[Totaal kostprijs])+
+SUM([Detail_OHW uitbesteed-werk-correcties_Bon].[Kostprijs correctie])
