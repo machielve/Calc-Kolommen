b53f8bc00baa8cea766861b87db2d5d170f0a863
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-03-29 16:29:13 +0200

    Go

--- Algemeen/Phone check.cs
+++ Algemeen/Phone check.cs
@@ -1,27 +1,7 @@
 If(
-Right(
-    Replace(
-        Replace(
-            Replace(
-                Replace( 
-                    [Directe telefoon],
-                "(0)",""), 
-            " " ,""),
-        "+","00"),
-    "-","")
-,5)
+Right( Replace( Replace( Replace( Replace( [Directe telefoon],          "(0)",""), " " ,""), "+","00"), "-","") ,5)
 ==
-Right(
-    Replace(
-        Replace(
-            Replace(
-                Replace( 
-                    [Relatie].[Telefoonnummer],
-                "(0)",""), 
-            " " ,""),
-        "+","00"),
-    "-","")
-,5)
+Right( Replace( Replace( Replace( Replace( [Relatie].[Telefoonnummer],  "(0)",""), " " ,""), "+","00"), "-","") ,5)
 AND
 [Directe telefoon]<>""
 
