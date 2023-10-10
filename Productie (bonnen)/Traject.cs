60f550f6f5eda3653fe67b22c025fb0346eb8810
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-10-10 09:10:43 +0200

    Go

--- Productie (bonnen)/Traject.cs
+++ Productie (bonnen)/Traject.cs
@@ -4,10 +4,12 @@ If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenhe
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==5 ,       "Externe voorraad L-Max", "")+
 
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort]<>"3" AND [Magazijn].[Magazijn]==2 
-AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")>0, "Productie ALM", "")+
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], 
+                        [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")>0, "Productie ALM", "")+
 
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2 
-AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")==0,        "Voorraad", "")+
+AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], 
+                        [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")==0,        "Voorraad", "")+
 
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]<=[@vrije voorraad] ,    "Voorraad", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", "")
