3c455f02cfe28833dba72f52561ef114c4fa8997
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-10-10 08:56:19 +0200

    Go

--- Productie (bonnen)/Traject op rapport.cs
+++ Productie (bonnen)/Traject op rapport.cs
@@ -1,6 +1,7 @@
 If([Artikel].[Voorraadtype]=="1",                                                                                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1",                                                                               "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==4,      "Uit voorraad bij BCB-Almacon", "")+
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==5,      "Uit voorraad bij Laser-Max", "")+
 
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==2 
 AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[Bonregel artikel uitbesteed werk id], [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")>0,      "Productie aangestuurd door Almacon", "")+
