64ac593218692a8fb712cd3144fe7f090bfcf0cf
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2023-10-19 15:23:14 +0200

    Go

--- Pakbon/Transport gewicht.cs
+++ Pakbon/Transport gewicht.cs
@@ -1,3 +1,32 @@
-SUMCONDITION(
+If([Transport].[Laad relatie]==395,
+    
+    SUMCONDITION(
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal],
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Detail_Alle reserveringen_Inkooporderregel divers].[Bonregel divers].[Bon]==[Bon] AND 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0002"),0.0)+
+
+
+
+If([Transport].[Laad relatie]==7,
+    
+    SUMCONDITION(
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal],
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Detail_Alle reserveringen_Inkooporderregel divers].[Bonregel divers].[Bon]==[Bon] AND 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0003"),0.0)+
+
+
+If([Transport].[Laad relatie]==548,
+    
+    SUMCONDITION(
     [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal], 
-    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0003")
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Detail_Alle reserveringen_Inkooporderregel divers].[Bonregel divers].[Bon]==[Bon] AND
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0004"),0.0)+
+
+If([Transport].[Laad relatie]==421,
+    
+    SUMCONDITION(
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Aantal], 
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Detail_Alle reserveringen_Inkooporderregel divers].[Bonregel divers].[Bon]==[Bon] AND
+    [Transport].[Detail_Inkooporderregel divers_Inkooporder].[Diversen].[Code]=="T0019"),0.0)
+
+
