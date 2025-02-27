1c4479b289a42d4fb152ccef6f9ce1f51a42f53e
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2025-02-27 14:50:02 +0100

    Update Werkbon check.cs

--- Productie (bonnen)/Checks/Werkbon check.cs
+++ Productie (bonnen)/Checks/Werkbon check.cs
@@ -1,4 +1,5 @@
 COUNTCONDITION([Order].[Detail_Documenten van order_Order].[Document].[Omschrijving], 
-[Order].[Detail_Documenten van order_Order].[Document].[Omschrijving]==ConvertIntToString([Order].[Ordernummer])+
-"-"+
-ConvertIntToString([Bonnummer])+"-WB")
+
+Left([Order].[Detail_Documenten van order_Order].[Document].[Omschrijving],9)
+==
+ConvertIntToString([Order].[Ordernummer])+"-"+ConvertIntToString([Bonnummer])+"-WB")
