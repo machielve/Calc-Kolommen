2a8cf00dc55ac7dd22f6ea17203d05704f3cd77b
Machiel van Emden <83112545+machielve@users.noreply.github.com>
2025-03-07 11:09:34 +0100

    Update Traject.cs

--- Productie (bonnen)/Traject.cs
+++ Productie (bonnen)/Traject.cs
@@ -1,3 +1,6 @@
+
+if([Registratietraject]=="1","Onbekend",
+
 If([Artikel].[Voorraadtype]=="1"                                                                    ,                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]<>"1"                                                ,                                   "Bestellen", "")+
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] <>"3"   AND [Magazijn].[Magazijn]==4 ,       "Externe voorraad BCB", "")+
@@ -12,5 +15,5 @@ AND COUNTCONDITION([Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel
                         [Detail_Bonregel artikel uitbesteed werk_KMB bonregel artikel].[KMB bonregel uitbesteed werk].[Uitbesteed werk].[Code]=="U1610")==0,        "Voorraad", "")+
 
 If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]<=[@vrije voorraad] ,    "Voorraad", "")+
-If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", "")
+If([Artikel].[Voorraadtype]=="4" AND [Zaagcode]=="1" AND [Artikel].[Artikeleenheid].[Soort] =="3"   AND [Aantal]>[@vrije voorraad] ,     "Productie ALM", ""))
 
